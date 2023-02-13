81ecd238530b8855bd1fd6c822cab16121a145d5
slowteady <vbn0213@gmail.com>
2023-02-13 22:22:58 +0900

    호출 파일 변경

--- 3.숫자야구/client.jsx
+++ 3.숫자야구/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import NumberBaseball from './NumberBaseball-class';
+import NumberBaseball from './NumberBaseball-function';
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<NumberBaseball />);
