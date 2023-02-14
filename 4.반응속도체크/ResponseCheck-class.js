1b662f25a5d61bff76b6e3c9371526e393a64e0d
slowteady <vbn0213@gmail.com>
2023-02-14 23:49:10 +0900

    결과 보여줄 평균값 처리 함수 구현

--- 4.반응속도체크/ResponseCheck-class.js
+++ 4.반응속도체크/ResponseCheck-class.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ResponseCheck extends Component {
+  state = {
+    state: 'waiting',
+    message: '클릭하여 시작하세요',
+    result: []
+  };
+
+  onClickScreen = () => {
+
+  }
+
+  renderAverage = () => {
+    const {result} = this.state;
+    return result.length === 0
+      ? null
+      : <>
+        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+        <button onClick={this.onReset}>리셋</button>
+      </>
+  };
 
   render() {
-    <>
-    
-    </>
+    const { state, message } = this.state; 
+    return (
+      <>
+        <div
+          id="screen"
+          className={state}
+          onClick={this.onClickScreen}
+        >
+          {message}
+        </div>
+        {this.renderAverage()}
+      </>
+    )
   }
 }
 
