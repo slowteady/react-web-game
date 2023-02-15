d6f3034553e7714e41066cd86be0b16a4d021dc3
slowteady <vbn0213@gmail.com>
2023-02-15 21:52:45 +0900

    반응속도체크 클래스 컴포넌트 추가 - 1차

--- 4.반응속도체크/ResponseCheck-class.js
+++ 4.반응속도체크/ResponseCheck-class.js
@@ -7,9 +7,47 @@ class ResponseCheck extends Component {
     result: []
   };
 
-  onClickScreen = () => {
+  timeout;
+  startTime;
+  endTime;
 
+  onClickScreen = () => {
+    const { state } = this.state;
+    if(state === 'waiting') {
+      timeout.current = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭!'
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요!'
+      });
+    } else if(state === 'ready') { // 잘못 클릭 했을 때
+      clearTimeout(this.timeout);
+      this.setState({
+        state: 'waiting',
+        message: '너무 일찍 클릭하셨어요!!!!! 하지만 빨랐죠?'
+      });
+    } else if(state === 'now') {
+      endTime.current = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭하여 시작하세요!',
+          result: [...prevState.result, this.endTime, this.startTime]
+        }
+      });
+    }
   }
+
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+  };
 
   renderAverage = () => {
     const {result} = this.state;
