7d03b9596dc4c733945a1ca52bc90353415f9697
slowteady <vbn0213@gmail.com>
2023-02-16 22:18:37 +0900

    클래스 컴포넌트 버전 재추가

--- 4.반응속도체크/ResponseCheck-class.js
+++ 4.반응속도체크/ResponseCheck-class.js
@@ -2,30 +2,61 @@ import React, { Component } from 'react';
 
 class ResponseCheck extends Component {
   state = {
-    state: 'ready',
+    state: 'waiting',
     message: '클릭하여 시작하세요',
     result: []
   }
 
+  timeout;
+  startTime;
+  endTime;
+
   onClickScreen = () => {
-    const { state, message, result } = this.state;
-    if(state === 'ready') {
+    const { state } = this.state;
+    if(state === 'waiting') {
       this.setState({
-        state: 'waiting',
+        state: 'ready',
         message: '초록색이 뜨면 클릭하세요',
       });
-    } else if(state === 'waiting') {
-      setTimeout(() => {
+      this.timeout = setTimeout(() => {
         this.setState({
           state: 'now',
           message: '클릭!'
         });
+        this.startTime = new Date();
       }, Math.floor(Math.random() * 1000) + 2000);
+    } else if(state === 'ready') {
+      clearTimeout(this.timeout);
+      this.setState({
+        state: 'waiting',
+        message: '너무 일찍 클릭했어요!!!'
+      });
+    } else if(state === 'now') {
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭하여 시작하세요',
+          result: [...prevState.result, this.endTime - this.startTime]
+        }
+      });
     }
   }
 
-  renderAverage = () => {
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+  };
 
+  renderAverage = () => {
+    const { result } = this.state;
+    return result.length === 0
+      ? null 
+      : <>
+        <div>평균 시간: { result.reduce((a,c) => a + c) / result.length } ms</div>
+        <button onClick={this.onReset}>리셋</button>
+      </>
   }
 
   render() {
