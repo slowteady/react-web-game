d078fac10ff7597b51893c9ed23dc09a103b78ef
slowteady <vbn0213@gmail.com>
2023-02-16 22:33:11 +0900

    함수컴포넌트 기본 구조 추가

---
+++ 4.반응속도체크/ResponseCheck-function.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const ResponseCheck = () => {
+
+  
+  return (
+    <>
+      <div id="screen" className={state} onClick={onClickScreen}>
+        {message}
+      </div>
+      {renderAverage()}
+    </>
+  );
+};
+
+export default ResponseCheck;
