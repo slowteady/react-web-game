f12154a69fbbd7cacff952bf3b7ed03b19a8fb8f
slowteady <vbn0213@gmail.com>
2023-02-18 00:00:09 +0900

    함수형 컴포넌트 진행중

--- 4.반응속도체크/ResponseCheck-function.js
+++ 4.반응속도체크/ResponseCheck-function.js
@@ -1,8 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const ResponseCheck = () => {
+  const [state, setState] = useState('waiting');
+  const [message, setMessage] = useState('클릭해서 시작하세요.');
+  const [result, setResult] = useState([]);
 
-  
+  const onClickScreen = () => {
+
+  }
+
+  const renderAverage = () => {
+    return result.length === 0
+      ? null
+      : <>
+        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+        <button onClick={onReset}>리셋</button>
+      </>
+  }; 
+
   return (
     <>
       <div id="screen" className={state} onClick={onClickScreen}>
