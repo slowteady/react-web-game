9b537596a3ef20c33963349c0eaac8b0d55600b2
slowteady <vbn0213@gmail.com>
2023-02-19 22:45:38 +0900

    number props 받아서 처리하는 컴포넌트 생성

--- 6.로또/Lotto-class.jsx
+++ 6.로또/Lotto-class.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import Ball from './Ball-class';
+
+function getWinNumbers() {
+  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const shuffle = [];
+  while(candidate.length > 0) {
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
 
 class Lotto extends Component {
+  state = {
+    winNumbers: getWinNumbers(),
+    winBalls: [],
+    bonus: null,
+    redo: false
+  }
 
   render() {
+    const { winBalls, bonus, redo } = this.state;
     return (
       <>
-        
+        <div>당첨숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => <Ball key={v} number={v} />)}
+        </div>
+        <div>보너스!</div>
       </>
     )
   }
