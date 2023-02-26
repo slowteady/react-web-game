94d7bd1a18db64f9ff858d899028d53d2689ab12
slowteady <vbn0213@gmail.com>
2023-02-26 17:46:25 +0900

    지뢰 렌더링 로직 추가

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -1,12 +1,92 @@
-import React from 'react';
+import React, { useReducer, createContext, useMemo } from "react";
+import Table from "./Table";
+import Form from "./Form";
+
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0,
+};
+
+export const TableContext = createContext({
+  tableData: [],
+  dispatch: () => {},
+});
+
+const initialState = {
+  tableData: [],
+  timer: 0,
+  result: "",
+};
+
+export const START_GAME = "START_GAME";
+
+const plantMine = (row, cell, mine) => {
+  const candidate = Array(row * cell)
+    .fill()
+    .map((arr, i) => {
+      return i;
+    });
+  const shuffle = [];
+  while (candidate.length > row * cell - mine) {
+    const chosen = candidate.splice(
+      Math.floor(Math.random() * candidate.length),
+      1
+    )[0];
+    shuffle.push(chosen);
+  }
+  const data = [];
+  for (let i = 0; i < row; i++) {
+    const rowData = [];
+    data.push(rowData);
+    for (let j = 0; j < cell; j++) {
+      rowData.push(CODE.NORMAL);
+    }
+  }
+
+  for (let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell);
+    const hor = shuffle[k] % cell;
+    data[ver][hor] = CODE.MINE;
+  }
+
+  return data;
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine),
+      };
+    default:
+      return state;
+  }
+};
 
 const MineSearch = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, timer, result } = state;
 
-  return (
-    <>
+  const value = useMemo(
+    () => ({ tableData, dispatch }),
+    [tableData]
+  );
     
-    </>
-  )
-}
+  return (
+    <TableContext.Provider value={value}>
+      <Form />
+      <div>{timer}</div>
+      <Table />
+      <div>{result}</div>
+    </TableContext.Provider>
+  );
+};
 
 export default MineSearch;
